56310763bf46afb6c8f5a25b67b25adf73b1b20a
Timothy Helton <timothy.j.helton@gmail.com>
2017-05-17 22:51:22 -0600

    FEAT: add Roots project and module

--- k2datascience/roots.py
+++ k2datascience/roots.py
@@ -5,12 +5,60 @@
 
 .. moduleauthor:: Timothy Helton <timothy.j.helton@gmail.com>
 """
+import logging
+import os
+import os.path as osp
+
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 
 
-class Indeed:
+log_format = ('%(asctime)s  %(levelname)8s  -> %(name)s <- '
+              '(line: %(lineno)d) %(message)s\n')
+date_format = '%m/%d/%Y %I:%M:%S'
+logging.basicConfig(format=log_format, datefmt=date_format,
+                    level=logging.INFO)
+
+current_dir = osp.dirname(osp.realpath(__file__))
+data_dir = osp.realpath(osp.join(current_dir, '..', 'data', 'roots'))
+census_cities = osp.join(data_dir, '2016_Gaz_zcta_national.txt')
+
+
+class LoadData:
+    """Load Data 
+    
+    :param str zipcode_file: data file with US zip codes
+    
+    :Attributes:
+    
+    """
+    def __init__(self, zip_code_file=census_cities):
+        self.zip_code_data = None
+        self.zip_code_file = zip_code_file
+
+    def __repr__(self):
+        return (f'LoadData('
+                f'zip_code_file={self.zip_code_file}')
+
+    def load_data(self):
+        """Load US city data."""
+        data_types = {
+            'zip_code': str,
+            'latitude': float,
+            'longitude': float,
+        }
+        self.zip_code_data = pd.read_table(
+            self.zip_code_file,
+            dtype=data_types,
+            encoding='ISO-8859-1',
+            header=0,
+            names=data_types.keys(),
+            usecols=[0, 5, 6],
+        )
+
+
+class Indeed(LoadData):
     """Attributes and methods related to querying indeed.com.
     
     :Attributes:
@@ -18,6 +66,7 @@ class Indeed:
     - **url**: *str* url address of target page to scrape
     """
     def __init__(self):
+        super().__init__()
         self.base_url = 'https://www.indeed.com/'
         self.jobs = None
         self.url = None
