65f06958e384f1a6b5157a75d6f15f5b177a41c0
Timothy Helton <timothy.j.helton@gmail.com>
2017-07-24 21:36:18 -0600

    ENH: add Resampling Exercises exercises 5

--- k2datascience/preprocessing.py
+++ k2datascience/preprocessing.py
@@ -8,12 +8,15 @@
 import logging
 import os.path as osp
 
-import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split, LeaveOneOut, KFold
+import pandas as pd
+import sklearn.linear_model as sklm
+import sklearn.model_selection as skms
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import PolynomialFeatures
 
-from k2datascience import plotting
+from k2datascience.utils import size, save_fig
 
 
 log_format = ('%(asctime)s  %(levelname)8s  -> %(name)s <- '
@@ -58,9 +61,11 @@ class Resample:
     - **response**: *Series* response of the model
     - **score**: *float* mean accuracy of the model
     - **test_size**: *float* percentage of data to be used for the test set
+    - **x**: *DataFrame* all feature samples
     - **x_train**: *DataFrame* training features
-    - **y_train**: *Series* training response
     - **x_test**: *DataFrame* testing features
+    - **y**: *DataFrame* all response samples
+    - **y_train**: *Series* training response
     - **y_test**: *Series* testing response
     """
     def __init__(self):
@@ -79,11 +84,26 @@ class Resample:
         self.response = None
         self.score = None
         self.test_size = 0.3
-
+        self.x = None
         self.x_test = None
         self.x_train = None
+        self.y = None
         self.y_test = None
         self.y_train = None
+
+    def linear_leave_one_out(self, degree=1):
+        """
+        Bootstrap for Leave One Out Cross Validation of Linear Regression.
+
+        :param int degree: the degree of the polynomial features to generate
+        """
+        loo = skms.LeaveOneOut()
+        model = make_pipeline(PolynomialFeatures(degree),
+                              sklm.LinearRegression())
+        scores = skms.cross_val_score(model, self.x, self.y,
+                                      scoring='neg_mean_squared_error', cv=loo)
+        print(f'Leave One Out Mean Square Error: {scores.mean() * -1:.3f} '
+              f'(+/- {scores.std():.3f})')
 
     def logistic_bootstrap(self, n):
         """
@@ -91,19 +111,25 @@ class Resample:
 
         :param int n: number of bootstrap datasets to generate.
         """
-        error_rates = []
-        for n in range(n):
-            self.validation_split()
-            self.logistic_regression(seed=n)
-            error_rates.append(self.error)
+        model = sklm.LogisticRegression()
+        scores = skms.cross_val_score(model, self.x, self.y, cv=n)
+        print(f'Validation Accuracy: {scores.mean() * 100:.3f}% '
+              f'(+/- {scores.std() * 100:.3f}%)')
+        print(f'Validation Error: {(1 - scores.mean()) * 100:.3f}%')
 
-        error = pd.Series(error_rates)
-        print(f'Error Rate Mean: {error.mean():.3f}')
-        print(f'Error Rate Median: {error.median():.3f}')
-        print(f'Error Rate Standard Deviation: {error.std():.3f}')
+    def logistic_leave_one_out(self, degree=1, seed=0):
+        """
+        Bootstrap for Leave One Out Cross Validation of Logistic Regression.
 
-        plotting.distribution_plot(error, 'Error Rate', 'Error Rate',
-                                   n_bins=20)
+        :param int degree: the degree of the polynomial features to generate
+        :param int seed: seed value to initialize the logistic regression model
+        """
+        loo = skms.LeaveOneOut()
+        model = make_pipeline(PolynomialFeatures(degree),
+                              sklm.LogisticRegression(random_state=seed))
+        scores = skms.cross_val_score(model, self.x, self.y, cv=loo)
+        print(f'Leave One Out Accuracy: {scores.mean() * 100:.2f}% '
+              f'(+/- {scores.std() * 100:.2f}%)')
 
     def logistic_regression(self, seed=0):
         """
@@ -111,7 +137,8 @@ class Resample:
 
         :param int seed: seed number for random state
         """
-        self.model = (LogisticRegression(C=1e5, tol=1e-7, random_state=seed)
+        self.model = (sklm.LogisticRegression(C=1e5, tol=1e-7,
+                                              random_state=seed)
                       .fit(self.x_train, self.y_train))
         self.intercept = self.model.intercept_
         self.coefficients = self.model.coef_
@@ -131,33 +158,16 @@ class Resample:
         print(f'Model Score: {self.score:.3f}')
         print(f'Error Rate: {self.error:.2f}%')
 
-    def leave_one_out(self):
-        """
-        Bootstrap for Leave One Out Cross Validation.
-        """
-        error_rates = []
-        x = pd.concat([self.x_train, self.x_test])
-        y = pd.concat([self.y_train, self.y_test])
-
-        loo = LeaveOneOut()
-        for n, idx in enumerate(loo.split(x)):
-            train_idx, test_idx = idx[0], idx[1]
-            self.x_train, self.x_test = x.iloc[train_idx], x.iloc[test_idx]
-            self.y_train, self.y_test = y.iloc[train_idx], y.iloc[test_idx]
-            self.logistic_regression(seed=n)
-            error_rates.append(self.error)
-
-        error = pd.Series(error_rates)
-        print(f'Error Rate Mean: {error.mean():.3f}')
-
     def validation_split(self):
         """
         Split the data based on a Validation set.
         """
         self.x_train, self.x_test, self.y_train, self.y_test = (
-            train_test_split(self.features, self.response,
-                             test_size=self.test_size)
+            skms.train_test_split(self.features, self.response,
+                                  test_size=self.test_size)
         )
+        self.x = pd.concat([self.x_train, self.x_test])
+        self.y = pd.concat([self.y_train, self.y_test])
 
 
 class LoanDefault(Resample):
@@ -179,6 +189,8 @@ class LoanDefault(Resample):
         self.features = self.data.loc[:, ['balance', 'income']]
         self.response = self.data.default.cat.codes
 
+        self.validation_split()
+
     def __repr__(self):
         return 'LoadDefault()'
 
@@ -192,6 +204,62 @@ class LoanDefault(Resample):
                                  names=self.data_types.keys(),
                                  skiprows=1,
                                  ))
+
+
+class Simulated(Resample):
+    """
+    Attributes and methods related to a simulated dataset.
+    """
+    def __init__(self):
+        super().__init__()
+        self.random_seed = 0
+        self.load_data()
+
+        self.features = self.data.x
+        self.response = self.data.y
+
+        self.validation_split()
+        self.single_feature()
+
+    def __repr__(self):
+        return 'Simulated()'
+
+    def load_data(self):
+        """
+        Load the data into a DataFrame
+        """
+        np.random.seed(self.random_seed)
+        x = np.random.randn(100)
+        e = np.random.randn(100)
+        y = x - 2 * x ** 2 + e
+        self.data = pd.DataFrame(np.c_[x, y],
+                                 columns=['x', 'y'])
+
+    def scatter_plot(self, save=False):
+        """
+        Create a scatter plot of the simulated data.
+
+        :param bool save: if True the figure will be saved
+        """
+        plt.figure('Simulated Scatter Plot', figsize=(10, 8),
+                   facecolor='white', edgecolor='black')
+        rows, cols = (1, 1)
+        ax0 = plt.subplot2grid((rows, cols), (0, 0))
+
+        self.data.plot(kind='scatter', x='x', y='y', alpha=0.5,
+                       edgecolor='black', ax=ax0)
+
+        ax0.set_title('Simulated Data Scatter Plot', fontsize=size['title'])
+        ax0.set_xlabel('X', fontsize=size['label'])
+        ax0.set_ylabel('Y', fontsize=size['label'])
+
+        save_fig('simulated_scatter', save)
+
+    def single_feature(self):
+        """
+        Add dimension to x attribute, since on one feature exists.
+        """
+        self.x = self.x.values.reshape(-1, 1)
 
 
 class StockMarket(Resample):
@@ -218,6 +286,8 @@ class StockMarket(Resample):
 
         self.features = self.data.loc[:, ['lag1', 'lag2']]
         self.response = self.data.direction.cat.codes
+
+        self.validation_split()
 
     def __repr__(self):
         return 'StockMarket()'
